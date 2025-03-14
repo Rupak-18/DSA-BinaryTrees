8475e4e1d3bb78ffc9ebffcc0daafa0e58915e0c
Rupak Das <rupak.das.3002@gmail.com>
2025-03-14 18:55:19 +0530

    Time: 0 ms (100%), Space: 42 MB (8.21%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,18 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    public void inorderHelper(TreeNode root, List<Integer> answer) {
-        if(root == null) 
-            return;
-        inorderHelper(root.left, answer);
-        answer.add(root.val);
-        inorderHelper(root.right, answer);
-    }
 
+// -------- RECURSIVE METHOD ---------
+
+// class Solution {
+//     public void inorderHelper(TreeNode root, List<Integer> answer) {
+//         if(root == null) 
+//             return;
+//         inorderHelper(root.left, answer);
+//         answer.add(root.val);
+//         inorderHelper(root.right, answer);
+//     }
+
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         inorderHelper(root, answer);
+//         return answer;
+//     }
+// }
+
+// -------- ITERATIVE METHOD ---------
+
+class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        inorderHelper(root, answer);
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null)
+            return answer;
+        TreeNode node = root;
+
+        while(true) {
+            if(node != null) {
+                st.push(node);
+                node = node.left;
+            }
+            else {
+                if(st.isEmpty())
+                    break;
+                node = st.pop();
+                answer.add(node.val);
+                node = node.right;
+            }
+        }
         return answer;
     }
 }
