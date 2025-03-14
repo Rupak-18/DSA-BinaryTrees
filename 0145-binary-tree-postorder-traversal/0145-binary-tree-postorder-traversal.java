a681a665f6bd4849e11f6440d8d694ef237194ea
Rupak Das <rupak.das.3002@gmail.com>
2025-03-14 21:06:55 +0530

    Time: 0 ms (100%), Space: 41.5 MB (82.25%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,18 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    public void postorderHelper(TreeNode root, List<Integer> answer) {
-        if(root == null) 
-            return;
-        postorderHelper(root.left, answer);
-        postorderHelper(root.right, answer);
-        answer.add(root.val);
-    }
 
+// ------ RECURSIVE METHOD (2 STACK) --------
+
+// class Solution {
+//     public void postorderHelper(TreeNode root, List<Integer> answer) {
+//         if(root == null) 
+//             return;
+//         postorderHelper(root.left, answer);
+//         postorderHelper(root.right, answer);
+//         answer.add(root.val);
+//     }
+
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         postorderHelper(root, answer);
+//         return answer;
+//     }
+// }
+
+// ------ ITERATIVE METHOD (2 STACK) --------
+
+class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        postorderHelper(root, answer);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        if(root == null)
+            return answer;
+
+        TreeNode node = root;
+        st1.push(node);
+
+        while(!st1.isEmpty()) {
+            node = st1.pop();
+            st2.push(node);
+            if(node.left != null)
+                st1.push(node.left);
+            if(node.right != null)
+                st1.push(node.right);
+        }
+        while(!st2.isEmpty()) 
+            answer.add(st2.pop().val);
         return answer;
     }
 }
