dc4b959e894ec64000af12a1d926449af896a244
Rupak Das <rupak.das.3002@gmail.com>
2025-03-14 18:30:09 +0530

    Time: 0 ms (100%), Space: 41.8 MB (44.53%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,18 +13,51 @@
  *     }
  * }
  */
+
+
+
+//---------- RECURSIVE METHOD ------------
+
+// class Solution {
+//     public void preorderHelper(TreeNode root, List<Integer> answer) {
+//         if(root == null) 
+//             return;
+//         answer.add(root.val);
+//         preorderHelper(root.left, answer);
+//         preorderHelper(root.right, answer);
+//     }
+
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         preorderHelper(root, answer);
+//         return answer;
+//     }
+// }
+
+
+//---------- ITERATIVE METHOD ------------
+
+
 class Solution {
-    public void preorderHelper(TreeNode root, List<Integer> answer) {
-        if(root == null) 
+
+    public void preOrderHelper(TreeNode root, List<Integer> answer) {
+        if(root == null)
             return;
-        answer.add(root.val);
-        preorderHelper(root.left, answer);
-        preorderHelper(root.right, answer);
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()) {
+            TreeNode node = st.pop();
+            answer.add(node.val);
+            if(node.right != null)
+                st.push(node.right);
+            if(node.left != null)
+                  st.push(node.left);    
+        }
     }
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        preorderHelper(root, answer);
+        preOrderHelper(root, answer);
         return answer;
     }
 }
