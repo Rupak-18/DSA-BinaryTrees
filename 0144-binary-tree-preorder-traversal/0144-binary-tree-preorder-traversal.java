a9d276687c0389cddfad9997f6080014d2dfa267
Rupak Das <rupak.das.3002@gmail.com>
2025-03-15 18:27:07 +0530

    Time: 1 ms (1.62%), Space: 41.4 MB (91.95%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -38,26 +38,74 @@
 //---------- ITERATIVE METHOD ------------
 
 
-class Solution {
+// class Solution {
 
-    public void preOrderHelper(TreeNode root, List<Integer> answer) {
-        if(root == null)
-            return;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()) {
-            TreeNode node = st.pop();
-            answer.add(node.val);
-            if(node.right != null)
-                st.push(node.right);
-            if(node.left != null)
-                st.push(node.left);    
-        }
+//     public void preOrderHelper(TreeNode root, List<Integer> answer) {
+//         if(root == null)
+//             return;
+//         Stack<TreeNode> st = new Stack<>();
+//         st.push(root);
+//         while(!st.isEmpty()) {
+//             TreeNode node = st.pop();
+//             answer.add(node.val);
+//             if(node.right != null)
+//                 st.push(node.right);
+//             if(node.left != null)
+//                 st.push(node.left);    
+//         }
+//     }
+
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         preOrderHelper(root, answer);
+//         return answer;
+//     }
+// }
+
+
+// -------- PREORDER, INORDER, POSTORDER IN 1 GO ----------
+
+class Pair {
+    TreeNode node;
+    int num;
+
+    public Pair(TreeNode node, int num) {
+        this.node = node;
+        this.num = num;
     }
+}
 
+class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        preOrderHelper(root, answer);
+        Stack<Pair> stack = new Stack<>();
+        if(root == null)
+            return answer;
+        stack.push(new Pair(root, 1));
+
+        while(!stack.isEmpty()) {
+            Pair current = stack.pop();
+
+            if(current.num == 1) {
+                answer.add(current.node.val);
+                current.num++;
+                stack.push(new Pair(current.node, current.num));
+                if(current.node.left != null)
+                    stack.push(new Pair(current.node.left, 1));
+            }
+            else if(current.num == 2) {
+                // push to inOrder answer
+                current.num++;
+                stack.push(new Pair(current.node, current.num));
+                if(current.node.right != null)
+                    stack.push(new Pair(current.node.right, 1));
+            }
+            else {
+                // push to postOrder answer
+                continue;
+            }     
+        }
         return answer;
     }
 }
+
